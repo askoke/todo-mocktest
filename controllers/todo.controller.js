ba9ebc4d54769b523308d370989b52b08fc804df
Asko <hulkko54@gmail.com>
2024-04-25 15:01:16 +0300

    Created TodoController.createTodo integration test and fixed unit test for using database

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,7 +1,7 @@
 const TodoModel = require('../models/todo.models')
 
-const createTodo = (req, res, next) => {
-    const createdModel = TodoModel.create(req.body)
+const createTodo = async (req, res, next) => {
+    const createdModel = await TodoModel.create(req.body)
     res.status(201).json(createdModel)
 }
 
